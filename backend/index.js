2febfade7daf1b76bf90f4e7669fb20e191d506c
ArCarixx <csa0031572@est.univalle.edu>
2024-10-01 23:10:03 -0400

    Vista informacion Paciente

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const port = 3000;
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -11,8 +11,8 @@ app.use(cors());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root123C!',
-    database: 'tuberculosis',
+    password: '1234',
+    database: 'mydb',
 });
 
 db.connect(err => {
@@ -44,3 +44,46 @@ app.post('/login', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+
+// Ruta para obtener todas las personas
+app.get('/personas', (req, res) => {
+  const query = 'SELECT * FROM persona';
+  
+  db.query(query, (err, result) => {
+    if (err) {
+      console.error('Error al ejecutar la consulta', err);
+      return res.status(500).send({ message: 'Error en el servidor' });
+    }
+    if (result.length === 0) {
+      return res.status(404).send({ message: 'No se encontraron personas' });
+    }
+    res.json(result);
+  });
+});
+
+// Ruta para obtener los datos de un paciente por su id
+app.get('/paciente/:id', (req, res) => {
+  const pacienteId = req.params.id;
+
+  const query = 'SELECT * FROM persona WHERE idPersona = ?';
+  db.query(query, [pacienteId], (err, result) => {
+    if (err) {
+      console.error('Error en la consulta:', err);  // Imprime el error en la consola
+      return res.status(500).send({ message: 'Error en el servidor' });
+    }
+    
+    if (result.length > 0) {
+      res.send(result[0]);  // Enviar solo el primer resultado
+    } else {
+      res.status(404).send({ message: 'Paciente no encontrado' });
+    }
+  });
+});
+
+
+
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando correctamente');
+});
+

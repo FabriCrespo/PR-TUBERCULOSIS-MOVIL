76b625504499671031ca3c7414cbe12d4d13f8ca
ArCarixx <csa0031572@est.univalle.edu>
2024-10-02 20:19:55 -0400

    Correccion pull

--- backend/index.js
+++ backend/index.js
@@ -44,3 +44,45 @@ app.post('/login', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+
+// Ruta para obtener todas las personas
+app.get('/personas', (req, res) => {
+  const query = 'SELECT * FROM persona';
+  
+  db.query(query, (err, result) => {
+    if (err) {
+      console.error('Error al ejecutar la consulta', err);
+      return res.status(500).send({ message: 'Error en el servidor' });
+    }
+    if (result.length === 0) {
+      return res.status(404).send({ message: 'No se encontraron personas' });
+    }
+    res.json(result);
+  });
+});
+
+// Ruta para obtener los datos de un paciente por su id
+app.get('/paciente/:id', (req, res) => {
+  const pacienteId = req.params.id;
+
+  const query = 'SELECT * FROM persona WHERE idPersona = ?';
+  db.query(query, [pacienteId], (err, result) => {
+    if (err) {
+      console.error('Error en la consulta:', err);  // Imprime el error en la consola
+      return res.status(500).send({ message: 'Error en el servidor' });
+    }
+    
+    if (result.length > 0) {
+      res.send(result[0]);  // Enviar solo el primer resultado
+    } else {
+      res.status(404).send({ message: 'Paciente no encontrado' });
+    }
+  });
+});
+
+
+
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando correctamente');
+});
